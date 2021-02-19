290cacfec149b7446a5116d833bb7bac333b2fee
yeri <yerihahn@gmail.com>
2021-02-19 19:53:08 +0900

    login

--- login/src/App.js
+++ login/src/App.js
@@ -1,17 +1,28 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="wrapper">
-        <div>
-          <h1>title</h1>
+        <div className="title">
+          <h1>Title</h1>
         </div>
-        <div>
-          <input type="text" />
+        <div className="input_wrap">
+          <div className="input_text input_id">
+            <input type="text" />
+          </div>
+          <div className="input_text input_pw">
+            <input type="text" />
+          </div>
         </div>
-        <div>
-          <input type="text" />
+        <div className="btn_click">
+          <button>Login</button>
+        </div>
+        <div className="checkwrap">
+          <label>
+            <input type="checkbox" />
+            ID 저장
+          </label>
         </div>
       </div>
     </div>
