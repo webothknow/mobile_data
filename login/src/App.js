ea8bc432446ee35359555de0fa22e98a305b9528
yeri <yerihahn@gmail.com>
2021-02-19 19:16:02 +0900

    파일삭제

--- login/src/App.js
+++ login/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="wrapper">
+        <div>
+          <h1>title</h1>
+        </div>
+        <div>
+          <input type="text" />
+        </div>
+        <div>
+          <input type="text" />
+        </div>
+      </div>
     </div>
   );
 }
